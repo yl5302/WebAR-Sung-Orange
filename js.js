914be3af1e2dfc2ec8dbe16b503801c97c6c381d
yl5302 <31666487+yl5302@users.noreply.github.com>
2020-09-03 16:10:35 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,46 @@
+
+             function block1() {
+             var x = document.getElementById("d_1");
+             x.style.display = "block";
+        
+			 }
+             function none1() {
+             var y = document.getElementById("d_1");
+             y.style.display = "none";
+        
+			 }
+    
+             function block2() {
+             var x = document.getElementById("d_2");
+             x.style.display = "block";
+        
+			 }
+             function none2() {
+             var y = document.getElementById("d_2");
+             y.style.display = "none";
+        
+			 }
+
+             
+             function block3() {
+             var x = document.getElementById("d_3");
+             x.style.display = "block";
+        
+			 }
+             function none3() {
+             var y = document.getElementById("d_3");
+             y.style.display = "none";
+        
+			 }
+
+             
+             function block4() {
+             var x = document.getElementById("d_4");
+             x.style.display = "block";
+        
+			 }
+             function none4() {
+             var y = document.getElementById("d_4");
+             y.style.display = "none";
+        
+			 }
